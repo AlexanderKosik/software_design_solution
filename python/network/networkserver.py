9d727c08af886f9f57506ca759e1c658478854a9
Alexander Kosik <alexander.kosik@aareal-bank.com>
2022-01-20 14:43:16 +0100

    Add docstring

--- python/network/networkserver.py
+++ python/network/networkserver.py
@@ -2,6 +2,12 @@ from socketserver import TCPServer
 from .handlerfactory import handler_factory
 
 class NetworkServerTCP:
+    """
+    This class is a TCP Network Server listening on passed ip and port
+
+    It can receive files and calls the passed callback if the file transfer 
+    is completed
+    """
     def __init__(self, ip, port, callback):
         self.serv = TCPServer((ip, port), handler_factory(callback))
         self.serv.serve_forever()
